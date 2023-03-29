cd0dd42a0445da7c0f2fa134b16cd3b507b8ed49
ValMtp <115800717+ValMtp3@users.noreply.github.com>
2023-03-29 16:46:21 +0200

    js

---
+++ Top.js
@@ -0,0 +1,18 @@
+// Afficher ou cacher le bouton en fonction de la position de défilement de la page
+window.addEventListener('scroll', function() {
+    var btn = document.getElementById('back-to-top-btn');
+    if (window.pageYOffset > 300) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  });
+  
+  // Faire défiler la page jusqu'en haut lorsque le bouton est cliqué
+  document.getElementById('back-to-top-btn').addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+  
