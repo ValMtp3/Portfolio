20ff5504c2a65d5995057fef3fb5470d89320cf1
ValMtp <115800717+ValMtp3@users.noreply.github.com>
2023-06-21 12:28:47 +0200

    amelioration importante

--- javascript.js
+++ javascript.js
@@ -1,19 +1,31 @@
 // Afficher ou cacher le bouton en fonction de la position de défilement de la page
-window.addEventListener("scroll", function () {
-  var btn = document.getElementById("back-to-top-btn");
-  if (window.pageYOffset > 300) {
-    btn.style.display = "block";
-  } else {
-    btn.style.display = "none";
-  }
-});
+// window.addEventListener("scroll", function () {
+//   var btn = document.getElementById("back-to-top-btn");
+//   if (window.pageYOffset > 300) {
+//    btn.style.display = "block";
+//   } else {
+//     btn.style.display = "none";
+//   }
+// });
 
 // Faire défiler la page jusqu'en haut lorsque le bouton est cliqué
-document
-  .getElementById("back-to-top-btn")
-  .addEventListener("click", function () {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
+// document
+//   .getElementById("back-to-top-btn")
+//   .addEventListener("click", function () {
+//     window.scrollTo({
+//       top: 0,
+//      behavior: "smooth",
+//    });
+//   });
+
+window.addEventListener("scroll", function () {
+  var t = document.getElementById("back-to-top-btn");
+  300 < window.pageYOffset
+    ? (t.style.display = "block")
+    : (t.style.display = "none");
+}),
+  document
+    .getElementById("back-to-top-btn")
+    .addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
     });
-  });
